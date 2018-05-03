bc54fca8ba6a6bde150aa8eaddfa81e3e4fe1458
Mohamed Moughamir <moughamir@gmail.com>
2018-05-03 18:34:07 +0100

    change logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          {/*
+          logo source : https://www.uplabs.com/posts/calculator-freebie-icons
+           */}
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Pretty Calculator</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
